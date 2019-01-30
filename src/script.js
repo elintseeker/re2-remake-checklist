744f16a6a8a2d9233f1ca04ca9bf0517fa2344d3
kevinn <knavia@gmail.com>
2019-01-30 21:16:57 +0800

    some refactoring

--- src/script.js
+++ src/script.js
@@ -1,4 +1,7 @@
-'use Stict';
+(function(){
+
+'use Strict';
+
 var standardList = document.getElementById('standard');
 var speedrunList = document.getElementById('speedrun');
 var hasStandardData = localStorage.getItem('re2StandardData');
@@ -32,29 +35,25 @@ var init = function(list) {
     'leon-b': {}
   };
 
-  if (standardList) {
-    console.log('standard run list...');
-    // check data
-    if (hasStandardData) {
-      restoreData();
-    }
-
-    return;
-  }
-
   if (speedrunList) {
     console.log('speedrun list...');
     // check data
     if (hasSpeedrunData) {
       restoreData();
     }
-    return;
+  } else {
+    console.log('standard run list...');
+    // check data
+    if (hasStandardData) {
+      restoreData();
+    }
   }
+
+  total.items();
 };
 
 
-// initialize
-init();
+
 
 var toggleItem = function(e) {
   var parentEl = this.parentNode.parentNode;
@@ -62,91 +61,117 @@ var toggleItem = function(e) {
   var dataId = parentEl.getAttribute('data-id');
 
   if (this.checked) {
-    console.log('tick', dataId);
+    console.log('Added', dataId);
     runData[sectionId][dataId] = true;
   } else {
-    console.log('untick', dataId);
-    // runData[dataId] = false;
+    console.log('Removed', dataId);
     delete runData[sectionId][dataId];
   }
 
-  // console.log(runData[sectionId].length);
+  // save to localStorage
   if (speedrunList) {
     localStorage.setItem('re2SpeedrunData', JSON.stringify(runData));
   } else {
     localStorage.setItem('re2StandardData', JSON.stringify(runData));
   }
 
-  countTotal(sectionId);
+  total.itemsChecked(sectionId);
 };
 
 
-
-var confirmDelete = function(e) {
-  var modal = document.querySelector('.modal');
-  modal.classList.add('is-active');
-};
-
-
-var closeModal = function(e) {
-  var modal = document.querySelector('.modal');
-  modal.classList.remove('is-active');
-};
-
-
-var deleteData = function(nuke) {
-  console.log('Deleting all data...');
-  var chkboxes = document.querySelectorAll('.checkbox input')
-  for ( var d = 0; d < chkboxes.length; d++ ) {
-    chkboxes[d].checked = false;
-  }
-
-  runData = {
-    'leon-a': {},
-    'claire-b': {},
-    'claire-a': {},
-    'leon-b': {}
-  };
-
-  if (nuke === 'all') {
-    console.log('Removing all checklist data...');
-    localStorage.clear();
-  } else {
-    console.log('deleting checklist...')
-    if (hasSpeedrunData) {
-      localStorage.removeItem('re2SpeedrunData');
-    } else {
-      localStorage.removeItem('re2StandardData');
+var modal = {
+  confirmDelete: function(e) {
+    var modal = document.querySelector('.modal');
+    modal.classList.add('is-active');
+  },
+  close: function(e) {
+    var modal = document.querySelector('.modal');
+    modal.classList.remove('is-active');
+  },
+  load: function() {
+    fetch('/modal.html')
+      .then(function(res){
+        // console.log(res);
+        return res.text(); // return text value;
+      })
+      .then(function(html){
+        document.getElementById('resetbar').innerHTML = html;
+        modal.events();
+      })
+      .catch(function(error){
+        console.log('error fetch!');
+      });
+  },
+  deleteData: function(nuke) {
+    console.log('Deleting all data...');
+    var chkboxes = document.querySelectorAll('.checkbox input')
+    for ( var d = 0; d < chkboxes.length; d++ ) {
+      chkboxes[d].checked = false;
     }
-  }
 
-  countTotal('reset');
-  closeModal();
+    runData = {
+      'leon-a': {},
+      'claire-b': {},
+      'claire-a': {},
+      'leon-b': {}
+    };
+
+    if (nuke === 'all') {
+      console.log('Removing all checklist data...');
+      localStorage.clear();
+    } else {
+      console.log('Deleting checklist...');
+      if (speedrunList) {
+        localStorage.removeItem('re2SpeedrunData');
+      } else {
+        localStorage.removeItem('re2StandardData');
+      }
+    }
+
+    total.items();
+    modal.close();
+  },
+  events: function() {
+    // event listeners to added modal;
+    var delAllDataEl = document.getElementById('resetAllChecklist');
+    delAllDataEl.addEventListener('click', function(){ modal.deleteData('all'); }, false);
+
+    var delChecklistEl = document.getElementById('resetChecklist');
+    delChecklistEl.addEventListener('click', function(){ modal.deleteData(); }, false);
+
+    var confirmBtn = document.getElementById('confirm');
+    confirmBtn.addEventListener('click', modal.confirmDelete, false);
+
+    var closeModalBtn = document.querySelector('.modal-close');
+    closeModalBtn.addEventListener('click', modal.close, false);
+  },
+};
+
+
+var total = {
+  items: function(){
+    var sections = document.querySelectorAll('section');
+    for (var i = 0; i < sections.length; i++) {
+      var thisId = sections[i].getAttribute('id');
+      var numItems = document.querySelectorAll('#' + thisId + ' input[type="checkbox"]').length;
+      var numItemsChecked = document.querySelectorAll('#' + thisId + ' input[type="checkbox"]:checked').length;
+
+      document.querySelector('[data-target="' + thisId + '"] .total').innerHTML = numItems;
+      total.itemsChecked(thisId);
+    }
+  },
+  itemsChecked: function(id) {
+    var items = document.querySelectorAll('#' + id + ' input[type="checkbox"]:checked');
+    var currTotal = document.querySelector('[data-target="' + id + '"] .currtotal');
+    currTotal.innerHTML = items.length;
+  }
 };
 
 
 var displaySection = function(e) {
   var elTarget = this.getAttribute('data-target');
   var tgt = document.getElementById(elTarget);
-  console.log(elTarget);
   tgt.classList.toggle('hide');
-};
-
-
-var countTotal = function(id) {
-  // count total number of ticked inputs
-  if (id === 'reset') {
-    var els = document.getElementsByClassName('currtotal');
-    console.log(els.length);
-
-    for (var z = 0; z < els.length; z++) {
-      els[z].innerHTML = '0';
-    }
-    return;
-  }
-
-  var currTotalEl = document.querySelector('[data-target="' + id + '"] .currtotal');
-  currTotalEl.innerHTML = Object.keys(runData[id]).length;
 };
 
 
@@ -162,47 +187,9 @@ for ( var i = 0; i < boxToggle.length; i++ ) {
   boxToggle[i].addEventListener('click', displaySection, false);
 }
 
-var sections = document.querySelectorAll('section');
-for (var x = 0; x < sections.length; x++ ) {
-  var id = sections[x].getAttribute('id');
-  var inputs = document.querySelectorAll('#' + id + ' input');
-  var totalItems = document.querySelector('[data-target="'+ id +'"] .total');
+document.addEventListener('DOMContentLoaded', modal.load, false);
 
-  totalItems.innerHTML = inputs.length;
-  countTotal(id);
-}
+// initialize
+init();
 
-
-var modalEvents = function() {
-  // event listeners to added modal;
-  var delAllDataEl = document.getElementById('resetAllChecklist');
-  delAllDataEl.addEventListener('click', function(){ deleteData('all'); }, false);
-
-  var delChecklistEl = document.getElementById('resetChecklist');
-  delChecklistEl.addEventListener('click', function(){ deleteData(); }, false);
-
-  var confirmBtn = document.getElementById('confirm');
-  confirmBtn.addEventListener('click', confirmDelete, false);
-
-  var closeModalBtn = document.querySelector('.modal-close');
-  closeModalBtn.addEventListener('click', closeModal, false);
-};
-
-
-// load modal html
-var loadModal = function() {
-  fetch('/modal.html')
-    .then(function(res){
-      // console.log(res);
-      return res.text(); // return text value;
-    })
-    .then(function(html){
-      document.getElementById('resetbar').innerHTML = html;
-      modalEvents();
-    })
-    .catch(function(error){
-      console.log('error fetch!');
-    });
-};
-
-document.addEventListener('DOMContentLoaded', loadModal, false);
+})();
